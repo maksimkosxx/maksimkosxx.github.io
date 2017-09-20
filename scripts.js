2a145b9e08fb63a8f205963b48a2f784d7a8712c
MScheglov <maksimkosxx@gmail.com>
2017-09-21 00:13:23 +0400

    Блок учителя

--- scripts.js
+++ scripts.js
@@ -5,5 +5,26 @@ $(document).ready(function () {
     });
 
 
-});
+    $('.video-btn').on("click", function(event) {
+
+        if ($(this).hasClass('play')) {
+            $(this).removeClass('play');
+            $(this).addClass('pause');
+
+            $('video').get(0).play();
+
+        } else {
+
+            $('.video-btn').removeClass('pause');
+            $(this).addClass('play');
+
+            var pl = $('video').get(0);
+            pl.pause();
+
+        }
+    });
+
+
+})
+;
 
