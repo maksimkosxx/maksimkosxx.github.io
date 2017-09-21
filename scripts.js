c4adb64c0615e2aeb75637d686ccdb4f20d7188c
MScheglov <maksimkosxx@gmail.com>
2017-09-21 23:33:18 +0400

    finish

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     });
 
 
-    $('.video-btn').on("click", function(event) {
+    $('.video-btn').on("click", function (event) {
 
         if ($(this).hasClass('play')) {
             $(this).removeClass('play');
@@ -24,6 +24,41 @@ $(document).ready(function () {
         }
     });
 
+    $('.about-us_slider, .reviews-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 700,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+
+    $('.header-navigation__link').click( function(){
+        $('.header-navigation__link').removeClass('active');
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('.profession-content__link').removeClass('active');
+            $(this).addClass('active');
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000);
+        }
+        return false;
+    });
+
+    $(window).scroll(function(){
+
+        if ($(window).scrollTop() > '300'){
+            $('.scroll-btn').css('display', 'block');
+        } else {
+            $('.scroll-btn').css('display', 'none');
+        }
+    });
+    $('.scroll-btn').click( function(){
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000);
+        }
+        return false;
+    });
 
 })
 ;
