05c5e70ca521edbcc919f249156e637d25244bee
Максим Щеглов <maksimkosxx@gmail.com>
2017-11-07 10:28:52 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $(".mobile-btn").on("click", function () {
-        $(this).toggleClass("active").animate(500), $(".nav").slideToggle()
+        $(this).toggleClass("active").animate(500);
+        $(".nav").slideToggle()
     }),
 
 
@@ -11,11 +12,6 @@ $(document).ready(function () {
         }),
 
 
-        // $(".btn--mouse").on("click", function () {
-        //     var a = $(this).attr("href");
-        //     return 0 != $(a).length && $("html, body").animate({scrollTop: $(a).offset().top}, 800), !1
-        // }),
-
         $(".video-btn").on("click", function () {
             if ($(this).hasClass("play")) $(this).removeClass("play"), $(this).addClass("pause"), $("video").get(0).play(); else {
                 $(".video-btn").removeClass("pause"), $(this).addClass("play");
@@ -23,50 +19,34 @@ $(document).ready(function () {
             }
         })
 
-
     $('#password-repeat').on('keyup', function () { // Выполняем скрипт при изменении содержимого 2-го поля
         var value_input1 = $("#password").val();
         var value_input2 = $(this).val();
         if (value_input1 != value_input2) {
-            $('.pass-repeat_label').html('Пароли не совпадают!').css('color','red');
+            $('.pass-repeat_label').html('Пароли не совпадают!').css('color', 'red');
             $('#submit').attr('disabled', 'disabled');
         } else {
             $('#submit').removeAttr('disabled');
-            $('.pass-repeat_label').html('Повторите пароль').css('color','#4d4d4d');
+            $('.pass-repeat_label').html('Повторите пароль').css('color', '#4d4d4d');
         }
     });
 
 
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 50
-    //     ){
-    //         $('.header').addClass('sticky');
-    //         $('.sticky').animate({
-    //             opacity: 1
-    //         }, 200);
-    //     }
-    //     else{
-    //         $('.header').removeClass('sticky');
-    //     }
-    // });
-
-
     // FULLPAGE
 
-    if ($(window).width() > 1500) {
+    if ($(window).width() > 2000) {
         $('#fullpage').fullpage({
             css3: true,
             scrollingSpeed: 700,
             autoScrolling: true
         });
+        $('section').removeClass('section--padding');
     } else {
-
+        $('section').addClass('section--padding');
     }
-    $('.btn--mouse').on('click', function(){
+    $('.btn--mouse').on('click', function () {
         $.fn.fullpage.moveSectionDown();
     });
-
-
 
 
 });
